ddb4dda7075e16589e5149d3bc204372617568c3
gustocruz <97600576+gustocruz@users.noreply.github.com>
2022-05-31 12:18:59 +0100

    Update streamlit_app.py
    typos

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard Boiled Free Range Egg')
-streamlit.text('🥑🍞 Avocato Toast')
+streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
@@ -16,7 +16,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #let's add a pick list
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocato', 'Strawberries'])
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 #display the table on te top page 
 streamlit.dataframe(my_fruit_list)
